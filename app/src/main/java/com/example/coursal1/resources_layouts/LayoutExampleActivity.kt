fcbd5e74e4cabae184aed20b7379b7c565d3b40e
Ecalle Thomas <thomasecalle@hotmail.fr>
2020-11-17 20:47:38 +0100

    Add 17/11 Add qualifiers examples

---
+++ app/src/main/java/com/example/coursal1/resources_layouts/LayoutExampleActivity.kt
@@ -0,0 +1,14 @@
+package com.example.coursal1.resources_layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coursal1.R
+
+class LayoutExampleActivity : AppCompatActivity()
+{
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_layout_example)
+  }
+}
