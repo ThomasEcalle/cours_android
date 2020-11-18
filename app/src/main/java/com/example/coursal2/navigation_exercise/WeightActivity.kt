0b7c83b8711f819f7818596e2b12a6a2bb71d709
Ecalle Thomas <thomasecalle@hotmail.fr>
2020-11-18 19:31:53 +0100

    Add 18/11 start exercise on navigation

---
+++ app/src/main/java/com/example/coursal2/navigation_exercise/WeightActivity.kt
@@ -0,0 +1,14 @@
+package com.example.coursal2.navigation_exercise
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.coursal2.R
+
+class WeightActivity : AppCompatActivity()
+{
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_weight)
+  }
+}
