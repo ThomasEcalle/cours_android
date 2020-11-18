435901bc70a51882fba69d6388c12e87336cc5e8
Ecalle Thomas <thomasecalle@hotmail.fr>
2020-11-18 19:55:37 +0100

    Add 18/11 correction exercise on navigation

--- app/src/main/java/com/example/coursal2/navigation_exercise/NameActivity.kt
+++ app/src/main/java/com/example/coursal2/navigation_exercise/NameActivity.kt
@@ -1,14 +1,44 @@
 package com.example.coursal2.navigation_exercise
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.coursal2.R
+import kotlinx.android.synthetic.main.activity_name.*
 
-class NameActivity : AppCompatActivity()
+class NameActivity : AppCompatActivity(), View.OnClickListener
 {
+  companion object
+  {
+    val REQUEST_CODE = 32
+  }
+
   override fun onCreate(savedInstanceState: Bundle?)
   {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_name)
+    validateName?.setOnClickListener(this)
+  }
+
+  override fun onClick(view: View?)
+  {
+    val name = nameEditText?.text.toString()
+    val intent = Intent(this, WeightActivity::class.java)
+    intent.putExtra(WeightActivity.NAME_PARAMETER_KEY, name)
+    startActivityForResult(intent, REQUEST_CODE)
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+  {
+    super.onActivityResult(requestCode, resultCode, data)
+    when (resultCode)
+    {
+      REQUEST_CODE ->
+      {
+        val weight = data?.getStringExtra(WeightActivity.RESULT_KEY)
+        validateName?.text = weight
+      }
+    }
   }
 }
--- app/src/main/java/com/example/coursal2/navigation_exercise/WeightActivity.kt
+++ app/src/main/java/com/example/coursal2/navigation_exercise/WeightActivity.kt
@@ -1,14 +1,38 @@
 package com.example.coursal2.navigation_exercise
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.coursal2.R
+import kotlinx.android.synthetic.main.activity_weight.*
 
-class WeightActivity : AppCompatActivity()
+class WeightActivity : AppCompatActivity(), View.OnClickListener
 {
+  companion object
+  {
+    val NAME_PARAMETER_KEY = "nameParameterKey"
+    val RESULT_KEY = "resultKey"
+  }
+
   override fun onCreate(savedInstanceState: Bundle?)
   {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_weight)
+
+    intent?.getStringExtra(NAME_PARAMETER_KEY)?.let {
+      nameTextView?.text = it
+    }
+
+    validateWeight?.setOnClickListener(this)
+  }
+
+  override fun onClick(p0: View?)
+  {
+    val weight = weightEditText?.text.toString()
+    val resultIntent = Intent()
+    resultIntent.putExtra(RESULT_KEY, weight)
+    setResult(NameActivity.REQUEST_CODE, resultIntent)
+    finish()
   }
 }
