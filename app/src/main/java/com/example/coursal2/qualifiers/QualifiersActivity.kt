4fe99eabab7af335395846b069db87189566d3ca
Ecalle Thomas <thomasecalle@hotmail.fr>
2020-12-15 17:36:13 +0100

    Add qualifiers lesson

---
+++ app/src/main/java/com/example/coursal2/qualifiers/QualifiersActivity.kt
@@ -0,0 +1,14 @@
+package com.example.coursal2.qualifiers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coursal2.R
+
+class QualifiersActivity : AppCompatActivity()
+{
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_qualifiers)
+  }
+}
